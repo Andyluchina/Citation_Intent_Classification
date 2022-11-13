50e314102650a0005b4526c904e6bc671b2e32e1
Qingjie Lu <qingjielu@Qingjies-MacBook-Air.local>
2022-11-13 17:07:12 -0500

    adding device checking scripts

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # run main models
+import torch
+
+device = None
+if torch.cuda.is_available():
+    print("Cuda is available, using CUDA")
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    print("MacOS acceleration is available, using MPS")
+    device = torch.device('mps')
+else:
+    print("No acceleration device detected, using CPU")
+    device = torch.device('cpu')
+
+# print(device)
