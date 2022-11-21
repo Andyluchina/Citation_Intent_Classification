20cf504ef4124958f6d4e9e4d4e7e36a19906e37
wl256 <wl256@duke.edu>
2022-11-21 01:56:56 -0500

    processing part

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,10 +1,33 @@
 # process two sets of data
 
-def filter(data):
-    out = {'text':[],'extended_context':[]}
-    for sample in data:
-        
+from collections import defaultdict, Counter, OrderedDict
 
 
 
-    return out
+def filter(data: list[dict], keys: list):
+    return [{key: value for (key, value) in sample.items() if key in keys} for sample in data]
+
+
+
+
+def counts_to_vocab(counter:dict, mfreq:int) -> tuple[list, dict]: 
+
+    UNK_SYMBOL = "<UNK>"
+    # SOS_SYMBOL = "<SOS>"
+    # EOS_SYMBOL = "<EOS>"
+    types = [UNK_SYMBOL] # SOS_SYMBOL, EOS_SYMBOL
+    types += [wtype for (wtype, wcount) in counter.most_common() if wcount >= mfreq]
+    type2idx = {wordtype: i for i, wordtype in enumerate(types)}
+    return types, type2idx
+
+
+def make_word_dict(data: list[dict], keys: list):
+
+    res = []
+    for key in keys:
+        word_counter = Counter()
+        for example in data:
+            word_counter.update(example[key].split())
+        res.append(word_counter)
+    return res
+
--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 import torch
 import json
 import numpy as np
-from data_preprocessing import *
+import data_preprocessing as processing
+from torchtext.vocab import GloVe
 
-
+glove_vectors = GloVe(name="6B", dim=100)
 
 # checking devices
 device = None
@@ -31,9 +32,33 @@ for x in f:
   y = json.loads(x)
   data.append(y)
 
-# processed_data = data_preprocessing.filter(data)
-
-for key, value in data[0].items():
-    print(key,': ', value,'\n\n')
 
 
+# all the dinctionary keys in data:
+# ['text', 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title', 'cited_paper_title', 'cited_author_ids', 
+# 'citing_author_ids', 'extended_context', 'section_number', 'section_title', 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 
+# 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index', 'section_name']
+
+kept_key = ['text','extended_context','intent','cleaned_cite_text','section_name']
+filtered_data = processing.filter(data, kept_key)
+
+keys = ['text','extended_context']
+word_dicts = processing.make_word_dict(filtered_data, keys=keys)
+
+text_dict, extended_context_dict = word_dicts
+
+
+input_types, input_types2idx = processing.counts_to_vocab(text_dict, mfreq=1)
+
+
+a = glove_vectors.get_vecs_by_tokens(input_types)
+count = 0
+for i, vec in enumerate(a):
+    if torch.sum(vec)==0:
+        count+=1
+        print(input_types[i],text_dict[input_types[i]])
+print(count)
+print(len(input_types))
+# print(a)
+
+# print(input_types)
