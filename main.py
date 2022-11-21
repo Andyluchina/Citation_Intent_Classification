b7eb7762df77406e1b9b49d9d93ab8f9ab394d01
Qingjie Lu <qingjielu@Qingjies-MacBook-Air.local>
2022-11-20 23:34:38 -0500

    commit before merge trials

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # run main models
 import torch
 import json
+from torchtext.vocab import GloVe, vocab
+import torchtext
+# glove_vectors = GloVe("6B", dim = 100)
 
 # checking devices
 device = None
@@ -19,16 +22,46 @@ else:
 ACL_TRAIN_PATH = './acl-arc/train.jsonl'
 data = []
 
-f = open(ACL_TRAIN_PATH, "r")
+SCICITE_TRAIN_PATH = './scicite/train.jsonl'
 
+# f = open(ACL_TRAIN_PATH, "r")
+#
+#
+# # dict_keys(['text',
+# # 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title',
+# # 'cited_paper_title', 'cited_author_ids', 'citing_author_ids', 'extended_context', 'section_number', 'section_title',
+# # 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index',
+# # 'section_name'])
+# for x in f:
+#   y = json.loads(x)
+#   data.append(y)
+#   print(y['text'])
+#   print(y['citing_paper_id'])
+#   print(y['section_name'])
+#   print(y['intent'])
+#   print(y['citing_paper_title'])
+#   print(y['extended_context'])
+#   print(y['section_number']) #maybe.....
+#   print(y['cite_marker_offset']) #where the interested citation is
+#   print(y['cleaned_cite_text'])
+#   # print(y['text'][134:148])
+#   break
+# # print(len(data))
+#
+f = open(SCICITE_TRAIN_PATH, "r")
+
+
+# dict_keys(['text',
+# 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title',
+# 'cited_paper_title', 'cited_author_ids', 'citing_author_ids', 'extended_context', 'section_number', 'section_title',
+# 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index',
+# 'section_name'])
 for x in f:
   y = json.loads(x)
   data.append(y)
-  print(y['text'])
-  break
-print(len(data))
-
-from torchtext.vocab import GloVe, vocab
-import torchtext
-
-glove_vectors = GloVe("6B", dim = 100)
+  print(y['string'])
+  s = y['citeStart']
+  e = y['citeEnd']
+  print(y['string'][s:e])
+  # print(y['text'][134:148])
+  # break
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 # model code
 class TransformerEncoder(nn.Module):
-  def __init__(self, enbedding_glove, dim_feedforward=512, head=8, num_layers=6, dropout=0.2, d_model=256):
+  def __init__(self, enbedding_glove, dim_feedforward=512, head=8, num_layers=3, dropout=0.2, d_model=256):
     """
     args:
       hidden_dim: hidden state size of LSTM
@@ -37,3 +37,25 @@ class TransformerEncoder(nn.Module):
     c_n = c_n.mean(0, keepdim=True)
 
     return self.dropout(lstm_out), (h_n, c_n)
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
