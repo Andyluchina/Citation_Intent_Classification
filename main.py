dde52c24774e6f3559f9b32812edfdbf7d4291c6
Andy Lu <qingjielu990916@gmail.com>
2022-11-21 01:40:23 -0500

    primary model ready

--- main.py
+++ main.py
@@ -24,31 +24,7 @@ data = []
 
 SCICITE_TRAIN_PATH = './scicite/train.jsonl'
 
-# f = open(ACL_TRAIN_PATH, "r")
-#
-#
-# # dict_keys(['text',
-# # 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title',
-# # 'cited_paper_title', 'cited_author_ids', 'citing_author_ids', 'extended_context', 'section_number', 'section_title',
-# # 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index',
-# # 'section_name'])
-# for x in f:
-#   y = json.loads(x)
-#   data.append(y)
-#   print(y['text'])
-#   print(y['citing_paper_id'])
-#   print(y['section_name'])
-#   print(y['intent'])
-#   print(y['citing_paper_title'])
-#   print(y['extended_context'])
-#   print(y['section_number']) #maybe.....
-#   print(y['cite_marker_offset']) #where the interested citation is
-#   print(y['cleaned_cite_text'])
-#   # print(y['text'][134:148])
-#   break
-# # print(len(data))
-#
-f = open(SCICITE_TRAIN_PATH, "r")
+f = open(ACL_TRAIN_PATH, "r")
 
 
 # dict_keys(['text',
@@ -57,11 +33,39 @@ f = open(SCICITE_TRAIN_PATH, "r")
 # 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index',
 # 'section_name'])
 for x in f:
-  y = json.loads(x)
-  data.append(y)
-  print(y['string'])
-  s = y['citeStart']
-  e = y['citeEnd']
-  print(y['string'][s:e])
+    y = json.loads(x)
+    data.append(y)
+    #   print(y['text'])
+    #   print(y['citing_paper_id'])
+    #   print(y['section_name'])
+    #   print(y['intent'])
+    #   print(y['citing_paper_title'])
+    #   print(y['extended_context'])
+    #   print(y['section_number']) #maybe.....
+    #   print(y['cite_marker_offset']) #where the interested citation is
+    #   print(y['cleaned_cite_text'])
+    if y['section_name'] == None:
+        print(y['text'])
+        print(y['extended_context'])
+        break
   # print(y['text'][134:148])
-  # break
+#   break
+# print(len(data))
+
+# f = open(SCICITE_TRAIN_PATH, "r")
+
+
+# dict_keys(['text',
+# 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title',
+# 'cited_paper_title', 'cited_author_ids', 'citing_author_ids', 'extended_context', 'section_number', 'section_title',
+# 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index',
+# 'section_name'])
+# for x in f:
+#   y = json.loads(x)
+#   data.append(y)
+#   print(y['string'])
+#   s = y['citeStart']
+#   e = y['citeEnd']
+#   print(y['string'][s:e])
+#   # print(y['text'][134:148])
+#   # break
--- model.py
+++ model.py
@@ -24,37 +24,38 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.size(0)]
         return self.dropout(x)
 
-class CustomTransformerEncoder(nn.Module):
-  def __init__(self, enbedding_glove, dim_feedforward=512, head=8, num_layers=3, dropout=0.2, d_model=256):
+class CustomTransformerClassifier(nn.Module):
+  def __init__(self, enbedding_glove, MAXLEN = 200, output_class_num = 4, dim_feedforward=512, head=8, num_layers=3, dropout=0.2, d_model=256, freeze_emb = False):
     """
     args:
-      hidden_dim: hidden state size of LSTM
-      dropout: this is applied to the output of the LSTM
+      enbedding_glove: pretrained glove embedding vector
     """
     ### Please do not change this function at all.
-    super(CustomTransformerEncoder, self).__init__()
+    super(CustomTransformerClassifier, self).__init__()
     self.dropout = nn.Dropout(dropout)
     self.pos_encoder = PositionalEncoding(d_model, dropout)
     # Embedding table over input vocabulary
-    self.embedder = nn.Embedding.from_pretrained(enbedding_glove,freeze = False)
+    self.embedder = nn.Embedding.from_pretrained(enbedding_glove,freeze = freeze_emb)
     encoder_layer = nn.TransformerEncoderLayer(d_model=d_model,dropout=dropout,dim_feedforward=dim_feedforward, nhead=head)
     self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
-
-  def forward(self, sentence):
+    self.input_projection = nn.Linear(enbedding_glove.shape[1], d_model)
+    self.output_linear = nn.Linear(MAXLEN*d_model,output_class_num)
+    self.logsoftmax = nn.LogSoftmax(dim=1)
+  def forward(self, sentence,src_mask = None,section_name = None):
     """
     args:
       sentence: [seq_len x batch_size] token indices
-
     returns:
-
+      output_probs [batch_size x output_class_num]
     """
     seq_len, batch_size = sentence.size()
-
-    # embedded_sentence: seq_len x batch_size x word_vector_dim
-    embedded_sentence = self.embedder(sentence)
+    # embedded_sentence: seq_len x batch_size x d_model
+    embedded_sentence = self.input_projection(self.embedder(sentence))
     pos_emb = self.pos_encoder(embedded_sentence)
-    # lstm_out: seq_len x batch_size x 2 * hidden_dim
-    # h_n, c_n: 2 x batch_size x hidden_dim
-    trans = self.transformer_encoder(pos_emb)
-
-    return trans
+    # trans: seq_len x batch_size x d_model
+    trans = self.transformer_encoder(pos_emb, src_mask)
+    trans_permute = torch.permute(trans, (1, 0, 2))
+    trans_flatten = torch.Flatten(trans_permute)
+    trans_output = self.output_linear(trans_flatten)
+    output_probs = self.logsoftmax(trans_output)
+    return output_probs
