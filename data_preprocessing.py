14b59d8092f84c8c3508bdc5d2a684a7a43c6a9b
wl256 <wl256@duke.edu>
2022-11-23 23:28:21 -0500

    add bert process

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,6 +6,87 @@ import re
 from torch.nn.utils.rnn import pad_sequence
 from torchtext.vocab import GloVe
 import numpy as np
+from transformers import BertTokenizer, BertModel
+
+
+
+
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-large-uncased')
+# model = BertModel.from_pretrained('bert-large-uncased')
+
+# text='CITATION'
+# text = "Typical examples are Bulgarian ( @Citation@ ; Simov and Osenova , 2003 ) , Chinese ( Chen et al. , 2003 ) , Danish ( Kromann , 2003 ) , and Swedish ( Nilsson et al. , 2005 ) . Second Sentence is here as well ."
+text = ["Typical examples are Bulgarian ( @Citation@ ; Simov and Osenova , 2003 ) , Chinese ( Chen et al. , 2003 ) , Danish ( Kromann , 2003 ) , and Swedish ( Nilsson et al. , 2005 ) . Second Sentence is here as well .",
+"Typical examples are Bulgarian ( @Citation@ ; Simov and Osenova , 2003 ) , Chinese ( Chen et al. , 2003 ) , Danish ( Kromann , 2003 ) , and Swedish ( Nilsson et al. , 2005 ) . Second Sentence is here as well ."]
+
+encoded_input = tokenizer(text, padding='max_length', max_length=100)
+for key,val in encoded_input.items():
+    print(key,val)
+
+# Convert token to vocabulary indices
+indexed_tokens = tokenizer.convert_tokens_to_ids(encoded_input)
+# print(indexed_tokens)
+
+
+
+
+class bert_process:
+    def __init__(self, data:list[dict], max_len:int=300, batch_size:int=1, shuffle:bool=True, pretrained_model_name:str='bert-large-uncased', padding:str='max_length'):
+        
+        self.data = data
+        self.max_len = max_len
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.padding = padding
+        self.tokenizer = BertTokenizer.from_pretrained(pretrained_model_name)
+        self.citation_id = 11091 # id for citation
+        self.cite_pos = [] #citation pisition
+        self.indexed_input = None
+        self.indexed_output = None
+        self.output_types2idx = {}
+        self.mask = None
+
+        self.index_input()
+        self.index_output()
+        self.make_data_loader()
+
+
+    def index_input(self):
+        
+        raw_input = []
+        for i, example in enumerate(self.data):
+            text, section_name = re.sub("@@CITATION", "@CITATION@", example['cleaned_cite_text']), example['section_name']
+            raw_input.append('section name : {} [SEP] sentence: {}'.format(section_name, text))
+
+        # input
+        encoded_input = self.tokenizer(raw_input, padding=self.padding, max_length=self.max_len) # dict
+        self.indexed_input = np.array(encoded_input['input_ids'])
+        self.mask = np.array(encoded_input['attention_mask'])
+        self.cite_pos = [x.index(self.citation_id) for x in self.encoded_input['input_ids']]
+
+
+    def index_output(self):
+        c = 0
+        self.indexed_output = []
+
+        for i, example in enumerate(self.data):
+            w = example['intent']
+            if self.output_types2idx.get(w, -1) == -1:
+                self.output_types2idx[w] = c
+                c += 1
+            self.indexed_output.append(self.output_types2idx[w])
+
+        self.indexed_output = np.array(self.indexed_output)
+
+
+
+    def make_data_loader(self):
+        dataset = Dataset(self.indexed_input, self.indexed_output, self.mask)
+        self.data_loader = torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=self.shuffle)
+
+
 
 
 
@@ -33,26 +114,61 @@ class Dataset:
 
 
 
-class Process:
 
 
-    def __init__(self, data:list[dict], input_key:list[str]=['section_name', 'cleaned_cite_text'], min_freq:int=1, max_len:int=250, 
-                batch_size:int=1, shuffle:bool=True, glove_name:str="6B",glove_dim:int=100):
+
+
+
+class glove_process:
+
+
+    def __init__(self, data:list[dict], input_key:list[str]=['section_name', 'cleaned_cite_text'], min_freq:int=1, max_len:int=300, 
+                batch_size:int=1, shuffle:bool=True, glove_name:str="6B", glove_dim:int=100):
 
 
         self.data = data # a list of dictionaries, one for one example/data point
         self.num_example = len(data) # number of data points in train/test/dev set
 
-    # all the dinctionary keys in data:
 
-    # ['text', 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title', 'cited_paper_title', 'cited_author_ids', 
-    # 'citing_author_ids', 'extended_context', 'section_number', 'section_title', 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 
-    # 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index', 'section_name']
+
+    # acl-arc data:
+
+    # keys:
+    # ['text', 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title', 'cited_paper_title', 'cited_author_ids', 'citing_author_ids', 'extended_context', 
+    # 'section_number', 'section_title', 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index', 'section_name']
 
     # relevant keys: ['text','extended_context','intent','cleaned_cite_text','section_name']
 
     # 'section_name': {'introduction' , 'experiments', None: , 'conclusion', 'related work', 'method'}
+
     # 'intent': {'Background', 'Uses', 'CompareOrContrast', 'Extends', 'Motivation', 'Future'}
+
+
+
+
+    # scicite data:
+
+    # keys: ['source', 'citeEnd', 'sectionName', 'citeStart', 'string', 'label', 'label_confidence', 'citingPaperId', 'citedPaperId', 'isKeyCitation', 'id', 'unique_id', 'excerpt_index']
+
+    # relevant keys: ['citeEnd', 'sectionName', 'citeStart', 'string', 'label']
+
+    # 'sectionName': 
+    # Counter({'Discussion': 1240, 'Introduction': 834, 'Methods': 800, '': 587, 'DISCUSSION': 483, 'Results': 359, '1. Introduction': 349, 'METHODS': 296, 'INTRODUCTION': 263, '4. Discussion': 172, 'RESULTS': 162, 
+    # '1 Introduction': 160, 'Background': 101, 'Method': 87, 'Results and discussion': 74, '2. Methods': 60, '1. INTRODUCTION': 54, 'Results and Discussion': 41, 'Methodology': 37, 'Materials and methods': 35, 
+    # 'RESULTS AND DISCUSSION': 33, '3. Discussion': 31, 'Materials and Methods': 30, '2 Methods': 25, nan: 19, '4 Experiments': 19, '3. Results': 17, 'Experimental design': 17, 'MATERIALS AND METHODS': 16, 
+    # '4. DISCUSSION': 16, '5. Discussion': 16, '4 Discussion': 16, '5 Experiments': 16, 'Implementation': 15, 'Present Address:': 13, '2. Method': 13, '3 Experiments': 12, 'METHOD': 11, '6 Experiments': 11, 
+    # '3. Methods': 11, '2 Related Work': 11, 'Experiments': 10, '4. Experiments': 10, '1 INTRODUCTION': 10, '3. Results and discussion': 9, 'Experimental Design': 7, '5. Experiments': 7, '3. Methodology': 7, 
+    # '2. METHODS': 7, '1. Background': 7, '2. Results and Discussion': 7, '2. Related Work': 7, '2 Related work': 7, 'METHODOLOGY': 7, 'Discussion and conclusions': 7, 'Technical considerations': 6, '3 Methodology': 6,
+    # '4 Implementation': 6, '3.2. Yield and characterisation of ethanol organosolv lignin (EOL)': 6, '3. Gaussian mixture and Gaussian mixture-of-experts': 6, '2 Method': 6, 
+    # 'Effects of Discourse Stages on Adjustment of Reference Markers': 6, 'Identification of lysine propionylation in five bacteria': 6, '6. Mitochondrial Lesions': 5, '2.2 Behavior and Mission of Autonomous System': 5,
+    # 'Structure and function of the placenta': 5, '5. A comparison with sound generation models': 5, 'Conclusions': 5, 'Role of Holistic Thinking': 5, '1.5 The Mystery of the Sushi Repeats: GABAB(1) Receptor Isoforms': 5, 
+    # '3. EXPERIMENTS': 5, 'MBD': 5, '3.1. Depression': 5, 'Implications for Explaining Cooperation: Towards a Classification in Terms of Psychological Mechanisms': 4, 'Interpretation Bias in Social Anxiety': 4, '7 Experiments': 4,
+    # '4 EXPERIMENTS': 4, '5.2. Implications for Formation of Crustal Plateau Structures': 4, '7 Numerical experiments': 4, 'Changes in CO2 and O3 Effects Through Time': 4, '3. A REAL OPTION TO INVEST UNDER CHOQUET-BROWNIAN AMBIGUITY': 4,
+    # .........
+
+
+    # 'label': Counter({'background': 4840, 'method': 2294, 'result': 1109})
+
 
 
         self.max_len = max_len # for batching, the max length of input sequence of one sample
@@ -82,10 +198,24 @@ class Process:
         self.pretrained_embedding = self.glove_vectors.get_vecs_by_tokens("<UNK>").view(1,-1) # initialize with UNK
         self.V = None
 
-        self.run() # call to run like a script
+        # Bert part
+        self.tokenizer = BertTokenizer.from_pretrained('bert-large-uncased', padding='right', max_length=self.max_len)
 
 
+        # self.run()
 
+
+# tokenizer = BertTokenizer.from_pretrained('bert-large-uncased', padding='right', max_length=20)
+# model = BertModel.from_pretrained('bert-large-uncased')
+
+# text = "Typical examples are Bulgarian ( @Citation@ ; Simov and Osenova , 2003 ) , Chinese ( Chen et al. , 2003 ) , Danish ( Kromann , 2003 ) , and Swedish ( Nilsson et al. , 2005 ) . Second Sentence is here as well ."
+# encoded_input = tokenizer(text)
+# # Convert token to vocabulary indices
+# # indexed_tokens = tokenizer.convert_tokens_to_ids(encoded_input)
+
+
+    # def bertProcess(self):
+    #     pass
 
 
     # make word counters for input sequences and output words in data
@@ -119,15 +249,15 @@ class Process:
 
     def build_input_vocab(self):
 
-        for word, count in self.input_word_counter.items():
+        for w, count in self.input_word_counter.items():
 
-            this_word = 'citation' if word == '@@citation' else word # search CITATION marker as 'citation' in Glove 
+            this_word = 'citation' if w == '@@citation' else w # search CITATION marker as 'citation' in Glove 
             vec = self.glove_vectors.get_vecs_by_tokens(this_word)
 
             if any([ele != 0 for ele in vec]) and count >= self.min_freq: # check if this word is in Glove and check its frequency
 
                 self.pretrained_embedding = torch.cat((self.pretrained_embedding, vec.view(1, -1)), 0)
-                self.input_types = np.append(self.input_types, word)
+                self.input_types = np.append(self.input_types, w)
 
         # get dict, mapping word to idx
         self.input_types2idx = {w: i for i, w in enumerate(self.input_types)}
@@ -187,9 +317,11 @@ class Process:
     def prepare_dataset(self):
 
         self.dataset = Dataset(self.padded_input, self.indexed_output, self.mask)
-
         self.data_loader = torch.utils.data.DataLoader(self.dataset, batch_size=self.batch_size, shuffle=self.shuffle)
 
+
+
+        
 
 
     def run(self):
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch
 import json
 import numpy as np
 from data_preprocessing import Process
-
+from collections import defaultdict, Counter, OrderedDict
 
 
 # checking devices
@@ -32,11 +32,100 @@ def load_data(path):
     return data
 
 
+
+
+
 ACL_TRAIN_PATH = './ACL-ARC/train.jsonl'
 ACL_TEST_PATH = './ACL-ARC/test.jsonl'
 ACL_DEV_PATH = './ACL-ARC/dev.jsonl'
 
-train_data, test_data, dev_data = load_data(ACL_TRAIN_PATH), load_data(ACL_TEST_PATH), load_data(ACL_DEV_PATH)
+# train_data, test_data, dev_data = load_data(ACL_TRAIN_PATH), load_data(ACL_TEST_PATH), load_data(ACL_DEV_PATH)
+
+
+
+
+
+
+SCICITE_TRAIN_PATH = './SCICITE/train.jsonl'
+SCICITE_TEST_PATH = './SCICITE/test.jsonl'
+SCICITE_DEV_PATH = './SCICITE/dev.jsonl'
+
+train_data, test_data, dev_data = load_data(SCICITE_TRAIN_PATH), load_data(SCICITE_TEST_PATH), load_data(SCICITE_DEV_PATH)
+
+# train: ~8k data points
+# 6k has label confidence
+# 4.3k label confidence >= 0.75
+# 3.4k label confidence = 1
+
+
+print(len(train_data))
+a=0
+b=0
+idx = []
+for i, x in enumerate(train_data):
+    if i==0:
+        print(x['string'])
+    try:
+        print("================")
+        print(x['string'][int(x['citeStart']):int(x['citeEnd'])], int(x['citeStart']), int(x['citeEnd']), len(x['string']))
+        if (int(x['citeEnd']) - int(x['citeStart'])) / len(x['string']) >= 0.4:
+            b+=1
+    except:
+        print("index nan")
+        idx.append(i)
+        a+=1
+        print(x['citeStart'], x['citeEnd'])
+
+
+    # try:
+    #     if x['label_confidence'] >= 0.7 and x['label_confidence'] < 1:
+    #         print(x['isKeyCitation'])
+    #         continue
+    #         # print(x)
+    #         # break
+    # except:
+    #     # print(x)
+    #     continue
+    # break
+
+print(a)
+print(b)
+# print(len(idx))
+
+
+# c=Counter()
+# for i,x in enumerate(train_data):
+#     c.update([x['sectionName']])
+
+# print(c)
+
+# Counter({'Discussion': 1240, 'Introduction': 834, 'Methods': 800, '': 587, 'DISCUSSION': 483, 'Results': 359, '1. Introduction': 349, 'METHODS': 296, 'INTRODUCTION': 263, '4. Discussion': 172, 'RESULTS': 162, 
+# '1 Introduction': 160, 'Background': 101, 'Method': 87, 'Results and discussion': 74, '2. Methods': 60, '1. INTRODUCTION': 54, 'Results and Discussion': 41, 'Methodology': 37, 'Materials and methods': 35, 
+# 'RESULTS AND DISCUSSION': 33, '3. Discussion': 31, 'Materials and Methods': 30, '2 Methods': 25, nan: 19, '4 Experiments': 19, '3. Results': 17, 'Experimental design': 17, 'MATERIALS AND METHODS': 16, 
+# '4. DISCUSSION': 16, '5. Discussion': 16, '4 Discussion': 16, '5 Experiments': 16, 'Implementation': 15, 'Present Address:': 13, '2. Method': 13, '3 Experiments': 12, 'METHOD': 11, '6 Experiments': 11, 
+# '3. Methods': 11, '2 Related Work': 11, 'Experiments': 10, '4. Experiments': 10, '1 INTRODUCTION': 10, '3. Results and discussion': 9, 'Experimental Design': 7, '5. Experiments': 7, '3. Methodology': 7, 
+# '2. METHODS': 7, '1. Background': 7, '2. Results and Discussion': 7, '2. Related Work': 7, '2 Related work': 7, 'METHODOLOGY': 7, 'Discussion and conclusions': 7, 'Technical considerations': 6, '3 Methodology': 6,
+# '4 Implementation': 6, '3.2. Yield and characterisation of ethanol organosolv lignin (EOL)': 6, '3. Gaussian mixture and Gaussian mixture-of-experts': 6, '2 Method': 6, 
+# 'Effects of Discourse Stages on Adjustment of Reference Markers': 6, 'Identification of lysine propionylation in five bacteria': 6, '6. Mitochondrial Lesions': 5, '2.2 Behavior and Mission of Autonomous System': 5,
+# 'Structure and function of the placenta': 5, '5. A comparison with sound generation models': 5, 'Conclusions': 5, 'Role of Holistic Thinking': 5, '1.5 The Mystery of the Sushi Repeats: GABAB(1) Receptor Isoforms': 5, 
+# '3. EXPERIMENTS': 5, 'MBD': 5, '3.1. Depression': 5, 'Implications for Explaining Cooperation: Towards a Classification in Terms of Psychological Mechanisms': 4, 'Interpretation Bias in Social Anxiety': 4, '7 Experiments': 4,
+# '4 EXPERIMENTS': 4, '5.2. Implications for Formation of Crustal Plateau Structures': 4, '7 Numerical experiments': 4, 'Changes in CO2 and O3 Effects Through Time': 4, '3. A REAL OPTION TO INVEST UNDER CHOQUET-BROWNIAN AMBIGUITY': 4,
+# ...........
+
+
+
+
+
+
+# d=Counter()
+# for i,x in enumerate(train_data):
+#     d.update([x['label']])
+
+# print(d)
+
+# Counter({'background': 4840, 'method': 2294, 'result': 1109})
+
+
 
 
 
@@ -44,14 +133,14 @@ train_data, test_data, dev_data = load_data(ACL_TRAIN_PATH), load_data(ACL_TEST_
 
 
 # create a class to process data and hold attributes
-train = Process(train_data, shuffle=False, batch_size=2)
+# train = Process(train_data, shuffle=False, batch_size=2)
 
 
 
-for j, (x_train, mask, y_train) in enumerate(train.data_loader):
-    # print(j,(x_train,mask, y_train))
-    print(np.take(train.input_types, x_train))
-    break
+# for j, (x_train, mask, y_train) in enumerate(train.data_loader):
+#     # print(j,(x_train,mask, y_train))
+#     print(np.take(train.input_types, x_train))
+#     break
 
 
 
