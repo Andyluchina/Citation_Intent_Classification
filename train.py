12eba0dcfa24b7bde9836e32425063d6a5e9b887
Andy Lu <qingjielu990916@gmail.com>
2022-11-24 04:49:04 -0500

    check correctness

--- train.py
+++ train.py
@@ -57,6 +57,7 @@ num_of_output = 6
 network = CustomBertClassifier(hidden_dim= 50, bert_dim_size=bert_dim_size, num_of_output=6)
 loss_fn = nn.NLLLoss()
 optimizer = torch.optim.Adam(network.parameters(), weight_decay=1e-4, lr=0.01)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 1, factor = 0.1, verbose = True)
 n_epochs = 60
 
 def evaluate_model(network, data):
@@ -106,6 +107,7 @@ for epoch in range(n_epochs):
     network.eval()
     print("dev loss and f1")
     curr_f1 = evaluate_model(network, dev_loader)
+    scheduler.step(curr_f1)
     if curr_f1 > best_f1:
         best_f1 = curr_f1
         torch.save(network.state_dict(), "bestmodel.npy")
