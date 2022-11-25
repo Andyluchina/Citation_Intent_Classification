6db39bf48dda02d0ad12458530d685d1407afc29
Andy Lu <qingjielu990916@gmail.com>
2022-11-25 02:32:34 -0500

    type id good, print params

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ test_loader = test.data_loader
 num_of_output = 6
 
 
-network = CustomBertClassifier(hidden_dim= 80, bert_dim_size=bert_dim_size, num_of_output=6)
+network = CustomBertClassifier(hidden_dim= 50, bert_dim_size=bert_dim_size, num_of_output=6)
 loss_fn = nn.NLLLoss()
 optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-3, lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.3, verbose = True)
