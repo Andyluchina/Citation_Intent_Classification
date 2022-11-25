e95ce6d5db8af3dc88129929af57308a31415f60
Andy Lu <qingjielu990916@gmail.com>
2022-11-25 18:14:28 -0500

    add class weight

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ num_of_output = 6
 
 
 network = CustomBertClassifier(hidden_dim= 80, bert_dim_size=bert_dim_size, num_of_output=6)
-loss_fn = nn.NLLLoss(weight=torch.tensor([1, 2, 3, 10, 9, 9]).to(device))
+loss_fn = nn.NLLLoss(weight=torch.tensor([1.0, 2.0, 3.0, 10.0, 9.0, 9.0]).to(device))
 # optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-3, lr=0.01)
 optimizer = torch.optim.Adam(network.parameters(), lr =0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 1, factor = 0.5, verbose = True)
