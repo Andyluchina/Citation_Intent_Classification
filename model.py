74376b70f01cdabe707e9a45a3d729a6ffddaff5
Andy Lu <qingjielu990916@gmail.com>
2022-11-24 19:12:25 -0500

    adding lstm

--- model.py
+++ model.py
@@ -7,13 +7,13 @@ from transformers import BertModel
 
 
 class CustomBertClassifier(nn.Module):
-    def __init__(self, hidden_dim= 200, bert_dim_size=768, num_of_output=6, lstm_hidden = 200, model_name = "bert-base-uncased"):
+    def __init__(self, hidden_dim= 200, bert_dim_size=768, num_of_output=6, lstm_hidden = 200,proj_size=50, model_name = "bert-base-uncased"):
         """
 
         """
         super(CustomBertClassifier, self).__init__()
         self.dropout = nn.Dropout(p=0.2)
-        self.linear1 = nn.Linear(4*lstm_hidden, hidden_dim)
+        self.linear1 = nn.Linear(300*2*proj_size, hidden_dim)
         self.linear2 = nn.Linear(hidden_dim, hidden_dim)
         self.linear3 = nn.Linear(hidden_dim, num_of_output)
         # self.bert_model = model
@@ -23,7 +23,7 @@ class CustomBertClassifier(nn.Module):
         for name, param in self.model.named_parameters():
             if 'classifier' not in name: # classifier layer
                 param.requires_grad = False
-        self.lstm = nn.LSTM(input_size=bert_dim_size, hidden_size=lstm_hidden, num_layers=3, batch_first=False, dropout=0.2, bidirectional=True)
+        self.lstm = nn.LSTM(input_size=bert_dim_size, hidden_size=lstm_hidden, num_layers=3, batch_first=False, dropout=0.2, bidirectional=True, proj_size=proj_size)
     def forward(self, sentences, citation_idxs, mask, device="mps"):
         """
         args:
@@ -47,15 +47,11 @@ class CustomBertClassifier(nn.Module):
         # bert_output: batch X seq_len X 2*bert_dim_size
         # print(bert_output.shape)
         
-        # print(citation_idxs)
-        citation_tokens = lstm_output[torch.arange(bert_output.shape[0]), citation_idxs]
-        # print(citation_tokens[0])
-        first_tokens = lstm_output[:, 0]
-        # print(bert_output[0, citation_idxs[0]])
-        # print(first_tokens.shape)
-        # print(citation_tokens.shape)
+        # citation_tokens = lstm_output[torch.arange(bert_output.shape[0]), citation_idxs]
+        # first_tokens = lstm_output[:, 0]
         # first_tokens batch X bert_dim_size
-        concat_tokens = torch.concat((first_tokens, citation_tokens), dim=1)
+        # concat_tokens = torch.concat((first_tokens, citation_tokens), dim=1)
+        concat_tokens = torch.Flatten(lstm_output)
         # concat_tokens = citation_tokens
         # concat_tokens batch X 2*bert_dim_size
         x1 = concat_tokens
