7265e27753f5ac66071d6ccdf55595f55ac66a2a
Andy Lu <qingjielu990916@gmail.com>
2022-11-27 00:26:22 -0500

    add weights back with absolute

--- model.py
+++ model.py
@@ -47,7 +47,7 @@ class CustomBertClassifier(nn.Module):
         bert_output = bert_output[0]
         # print(bert_output[:, -1].shape)
         # mask = mask.type(torch.Tensor).to(device)
-        lstm_output = self.transformer_encoder(self.linear_bert(self.dropout(bert_output)))
+        lstm_output = self.lstm(self.linear_bert(self.dropout(bert_output)))
         lstm_output = lstm_output[0]
         # print(lstm_output.shape)
         # lstm_output: batch X seq_len X 2*bert_dim_size
