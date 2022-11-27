660a2bfbd7684f47dc290056cf7126e76570634f
Andy Lu <qingjielu990916@gmail.com>
2022-11-26 20:02:05 -0500

    changing things to transformer

--- train.py
+++ train.py
@@ -47,7 +47,7 @@ SCICITE_DEV_PATH = './scicite/dev.jsonl'
 train_data_sci, test_data_sci, dev_data_sci = load_data(SCICITE_TRAIN_PATH), load_data(SCICITE_TEST_PATH), load_data(SCICITE_DEV_PATH)
 
 # train_data, test_data, dev_data = train_data[:40], test_data, dev_data
-bz = 128
+bz = 64
 bertmodel_name = 'bert-base-uncased'
 bert_dim_size = 768
 
@@ -70,9 +70,10 @@ network = CustomBertClassifier(hidden_dim= 100, bert_dim_size=bert_dim_size, num
 
 # loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,2.735015773,2.842622951,13.76190476,11.40789474,14.45]).to(device))
 # 1	10.1829653	7.017391304	51.23809524	42.47368421	53.8
-loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,10.1829653,7.017391304,51.23809524,42.47368421,53.8]).to(device))
-# optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-3, lr=0.01)
-optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
+# loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,10.1829653,7.017391304,51.23809524,42.47368421,53.8]).to(device))
+loss_fn = nn.NLLLoss()
+optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-3, lr=0.01)
+# optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 200
 
