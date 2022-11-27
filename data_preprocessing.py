5b5624ca891e96fac2574ee4d304adcade5e4884
Andy Lu <qingjielu990916@gmail.com>
2022-11-27 05:18:46 -0500

    add weights for now

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -38,6 +38,8 @@ class bert_process:
 
         self.indexed_input = None
         self.indexed_output = None
+        #  {'0': 3328, '1': 317, '2': 460, '3': 63, '4': 76, '5': 60}
+        # {'Background':3, 'Uses':1, 'CompareOrContrast':2, 'Extends':4, 'Motivation':0, 'Future':5}
         self.output_types2idx = {'Background':0, 'Uses':1, 'CompareOrContrast':2, 'Extends':3, 'Motivation':4, 'Future':5}
         self.mask = None
         self.token_type_ids = None
