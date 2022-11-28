cbc14075476b4ca5b5e2ddcbd90f4433690e4a9a
Andy Lu <qingjielu990916@gmail.com>
2022-11-28 18:50:18 -0500

    smaller model remove scicite

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-6, lr=0.000
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 60
-class_factor = 0.8
+class_factor = 0.7
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
 # for parameter in network.parameters():
