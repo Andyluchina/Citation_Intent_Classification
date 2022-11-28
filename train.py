a1d2188a66fda72fb5ed3acc8224a74e6fe9cbba
Andy Lu <qingjielu990916@gmail.com>
2022-11-28 16:37:02 -0500

    smaller model remove scicite

--- train.py
+++ train.py
@@ -78,7 +78,7 @@ network = CustomBertClassifier(hidden_dim= 100, bert_dim_size=bert_dim_size, num
 # loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,10.1829653,7.017391304,51.23809524,42.47368421,53.8]).to(device))
 loss_fn = nn.NLLLoss()
 
-optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.00005)
+optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.0001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 60
