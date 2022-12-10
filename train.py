2e53c6dd05d0dcda132b1182a97f1adbaf740cdf
Andy Lu <qingjielu990916@gmail.com>
2022-12-10 20:17:52 +0800

    changing things to squared

--- train.py
+++ train.py
@@ -112,7 +112,7 @@ def evaluate_model(network, data, data_object):
         # loss = F.nll_loss(output, y, weight=torch.tensor([1.0, 500.151702786,700.234782609,4300.78947368,5200.82539683,5500.46666667]).to(device))
         
         _, predicted = torch.max(output, dim=1)
-        loss = loss_fn(output, y) + class_factor * ((torch.subtract(y, predicted) != 0).sum()) + sum_factor * torch.sum(torch.absolute(torch.subtract(y, predicted)))
+        loss = loss_fn(output, y) + class_factor * (torch.square(torch.subtract(y, predicted)).sum())
 
         f1 = F1Score(num_classes=num_of_output, average='macro').to(device)
         # self.output_types2idx = {'Background':3, 'Uses':1, 'CompareOrContrast':2, 'Extends':4, 'Motivation':0, 'Future':5}
@@ -173,7 +173,7 @@ for epoch in range(n_epochs):
         # loss = loss_fn(output, y) + class_factor * torch.absolute(torch.sum(y) - torch.sum(predictted_output))
         # if epoch < 15:    
         # loss = loss_fn(output, y) + class_factor * ((torch.subtract(y, predictted_output) != 0).sum()) + sum_factor * torch.sum(torch.absolute(torch.subtract(y, predictted_output)))
-        loss = loss_fn(output, y) + class_factor * torch.subtract(torch.sum(y), torch.sum(predictted_output)) + sum_factor * torch.sum(torch.absolute(torch.subtract(y, predictted_output)))
+        loss = loss_fn(output, y) + class_factor * (torch.square(torch.subtract(y, predictted_output)).sum())
         # loss = loss_fn(output, y) + torch.exp(class_factor * torch.sum(torch.absolute(torch.subtract(y, predictted_output))))
         # else:
         #     # loss = loss_fn(output, y) + class_factor * max(0.1,1/((epoch-13)/2)) * torch.sum(torch.absolute(torch.subtract(y, predictted_output)))
