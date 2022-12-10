4962c57b3929f951c07d59df6e4da344a63ee110
Andy Lu <qingjielu990916@gmail.com>
2022-12-10 20:36:52 +0800

    adding batch size

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-7, lr=0.000
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 60
-class_factor = 0.4
+class_factor = 0.7
 sum_factor = 0.8
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
