d32a10fef4d21b30ee1840a23db7094c36351729
Andy Lu <qingjielu990916@gmail.com>
2022-12-12 21:35:16 +0800

    changing L2

--- train.py
+++ train.py
@@ -82,7 +82,7 @@ optimizer = torch.optim.Adam(network.parameters(), weight_decay = 3e-6, lr=0.001
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 80
-class_factor = 1.8
+class_factor = 4
 sum_factor = 0.8
 accuracy_factor = 5
 
