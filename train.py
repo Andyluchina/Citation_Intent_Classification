ff0089cce6731faa770ea5fceef5525794a7fa31
Andy Lu <qingjielu990916@gmail.com>
2022-12-12 20:10:05 +0800

    change norm l2

--- train.py
+++ train.py
@@ -78,10 +78,10 @@ network = CustomBertClassifier(hidden_dim= 100, bert_dim_size=bert_dim_size, num
 # loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,1.0,1.0,1.5,1.5,1.5]).to(device))
 loss_fn = nn.NLLLoss()
 
-optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.001)
+optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-6, lr=0.001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
-n_epochs = 60
+n_epochs = 80
 class_factor = 1.8
 sum_factor = 0.8
 accuracy_factor = 5
