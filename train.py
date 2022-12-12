510f731291f8877a7e3dca4eec9f2276aef1300d
Andy Lu <qingjielu990916@gmail.com>
2022-12-12 16:14:14 +0800

    change data preprocessing

--- train.py
+++ train.py
@@ -75,10 +75,10 @@ network = CustomBertClassifier(hidden_dim= 100, bert_dim_size=bert_dim_size, num
 
 # loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,2.735015773,2.842622951,13.76190476,11.40789474,14.45]).to(device))
 # 1	10.1829653	7.017391304	51.23809524	42.47368421	53.8
-# loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,1.0,1.0,2.0,1.7,2.0]).to(device))
-loss_fn = nn.NLLLoss()
+loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,1.0,1.0,1.5,1.5,1.5]).to(device))
+# loss_fn = nn.NLLLoss()
 
-optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.001)
+optimizer = torch.optim.Adam(network.parameters(), weight_decay = 5e-6, lr=0.001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 60
