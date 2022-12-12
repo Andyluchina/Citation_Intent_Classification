d0e2b3a10845845ea12892bcd91eeb8689188dd3
Andy Lu <qingjielu990916@gmail.com>
2022-12-12 15:46:57 +0800

    change data preprocessing

--- train.py
+++ train.py
@@ -115,7 +115,7 @@ def evaluate_model(network, data, data_object):
         _, predicted = torch.max(output, dim=1)
         loss = accuracy_factor * loss_fn(output, y) /  ((torch.subtract(y, predicted) != 0).sum())+ class_factor * torch.sum(torch.square(torch.subtract(y, predicted)))
         print("Accuracy Loss: ", accuracy_factor * loss_fn(output, y) /  ((torch.subtract(y, predicted) != 0).sum()))
-        print("Class Loss: ", class_factor * torch.sum(torch.sqaure(torch.subtract(y, predicted))))
+        print("Class Loss: ", class_factor * torch.sum(torch.square(torch.subtract(y, predicted))))
         f1 = F1Score(num_classes=num_of_output, average='macro').to(device)
         # self.output_types2idx = {'Background':3, 'Uses':1, 'CompareOrContrast':2, 'Extends':4, 'Motivation':0, 'Future':5}
         for x in y.cpu().detach().tolist():
