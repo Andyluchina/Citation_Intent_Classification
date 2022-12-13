35e1d67228f0c28d53d5e26130b49d535f5ea632
Andy Lu <qingjielu990916@gmail.com>
2022-12-13 22:51:27 +0800

    change the factor

--- train.py
+++ train.py
@@ -88,7 +88,7 @@ n_epochs = 80
 class_factor = 0.9
 sum_factor = 0.8
 normalizing_factor = 1
-accuracy_factor = 1.2
+accuracy_factor = 1.4
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
 # for parameter in network.parameters():
@@ -117,7 +117,7 @@ def evaluate_model(network, data, data_object):
         # loss = F.nll_loss(output, y, weight=torch.tensor([1.0, 500.151702786,700.234782609,4300.78947368,5200.82539683,5500.46666667]).to(device))
         
         _, predicted = torch.max(output, dim=1)
-        loss = accuracy_factor * loss_fn(output, y) /  torch.log(torch.absolute(torch.subtract(y, predicted)).sum())+ class_factor * ((torch.subtract(y, predicted) != 0).sum()) + class_factor * torch.square(torch.subtract(y, predicted)).sum()
+        loss = accuracy_factor * loss_fn(output, y) /  torch.log(torch.absolute(torch.subtract(y, predicted)).sum()) + class_factor * torch.square(torch.subtract(y, predicted)).sum()
         print("Accuracy Loss: ", accuracy_factor * loss_fn(output, y) /  torch.log(torch.absolute(torch.subtract(y, predicted)).sum()))
         print("Class Loss: ", class_factor * ((torch.subtract(y, predicted) != 0).sum()))
         f1 = F1Score(num_classes=num_of_output, average='macro').to(device)
@@ -181,7 +181,7 @@ for epoch in range(n_epochs):
         # loss = loss_fn(output, y) + class_factor * torch.absolute(torch.sum(y) - torch.sum(predictted_output))
         # if epoch < 15:    
         # loss = loss_fn(output, y) + class_factor * ((torch.subtract(y, predictted_output) != 0).sum()) + sum_factor * torch.sum(torch.absolute(torch.subtract(y, predictted_output)))
-        loss = accuracy_factor * loss_fn(output, y) / torch.log(torch.absolute(torch.subtract(y, predictted_output)).sum()) + class_factor * ((torch.subtract(y, predictted_output) != 0).sum()) + class_factor * torch.square(torch.subtract(y, predictted_output)).sum()
+        loss = accuracy_factor * loss_fn(output, y) / torch.log(torch.absolute(torch.subtract(y, predictted_output)).sum()) + class_factor * torch.square(torch.subtract(y, predictted_output)).sum()
         # loss = loss_fn(output, y) + torch.exp(class_factor * torch.sum(torch.absolute(torch.subtract(y, predictted_output))))
         # else:
         #     # loss = loss_fn(output, y) + class_factor * max(0.1,1/((epoch-13)/2)) * torch.sum(torch.absolute(torch.subtract(y, predictted_output)))
