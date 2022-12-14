0a54efb3db3e26008873c008bab911df47a4a135
Andy Lu <qingjielu990916@gmail.com>
2022-12-14 21:04:19 +0800

    revert the params

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ else:
     bert_dim_size = 1024
 
 
-repeat = [1,1,1,5,4,3]
+repeat = [1,1,1,5,4,1]
 
 # train = bert_process(train_data, batch_size=bz, pretrained_model_name=bertmodel_name)
 train = bert_process(train_data, train_data_sci ,batch_size=bz, pretrained_model_name=bertmodel_name, repeat=repeat)
@@ -87,11 +87,11 @@ loss_fn = nn.NLLLoss()
 optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 3, factor = 0.5, verbose = True)
-n_epochs = 45
+n_epochs = 80
 class_factor = 1.4
 sum_factor = 0.8
 normalizing_factor = 1
-accuracy_factor = 8
+accuracy_factor = 1.8
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
 # for parameter in network.parameters():
