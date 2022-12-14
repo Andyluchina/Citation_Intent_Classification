f65edd85b2eeadf3e9c6486c0f313f73ffa8fbc3
Andy Lu <qingjielu990916@gmail.com>
2022-12-14 21:49:45 +0800

    revert to the best model

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ loss_fn = nn.NLLLoss()
 optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
-n_epochs = 80
+n_epochs = 35
 class_factor = 1.4
 sum_factor = 0.8
 normalizing_factor = 1
