a2ebac19837480e400b14f2b2a377c3f79c9776f
Andy Lu <qingjielu990916@gmail.com>
2022-12-14 17:03:25 +0800

    remove train loss

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ n_epochs = 80
 class_factor = 1.4
 sum_factor = 0.8
 normalizing_factor = 1
-accuracy_factor = 5
+accuracy_factor = 8
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
 # for parameter in network.parameters():
