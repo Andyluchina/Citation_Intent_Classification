94d7044349dc159acc0ec35d08faa447c91fe097
Andy Lu <qingjielu990916@gmail.com>
2022-12-14 22:28:24 +0800

    minor adjust params

--- train.py
+++ train.py
@@ -84,14 +84,14 @@ network = CustomBertClassifier(hidden_dim= 100, bert_dim_size=bert_dim_size, num
 # loss_fn = nn.NLLLoss(weight=torch.tensor([1.0,1.0,1.0,1.5,1.5,1.5]).to(device))
 loss_fn = nn.NLLLoss()
 
-optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.001)
+optimizer = torch.optim.Adam(network.parameters(), weight_decay = 2e-5, lr=0.001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 35
-class_factor = 1.7
+class_factor = 1.9
 sum_factor = 0.8
 normalizing_factor = 1
-accuracy_factor = 1.2
+accuracy_factor = 1.4
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
 # for parameter in network.parameters():
