c0cc37dd3c6e26785caf3cf3ea1a1c7766931ac3
Andy Lu <qingjielu990916@gmail.com>
2022-12-14 13:52:19 +0800

    changing incentive

--- train.py
+++ train.py
@@ -83,12 +83,12 @@ loss_fn = nn.NLLLoss()
 
 optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 3, factor = 0.5, verbose = True)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 80
 class_factor = 1.2
 sum_factor = 0.8
 normalizing_factor = 1
-accuracy_factor = 5
+accuracy_factor = 2
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
 # for parameter in network.parameters():
