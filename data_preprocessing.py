bfdacb221f78da4deeac22c5c4414ebc79db6538
Andy Lu <qingjielu990916@gmail.com>
2022-12-16 14:05:46 +0800

    change data preprocessing to work with repaet

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,6 +6,7 @@ import re
 from torch.nn.utils.rnn import pad_sequence
 import numpy as np
 from transformers import BertTokenizer
+# from transformers import *
 import json
 import matplotlib.pyplot as plt
 
@@ -15,7 +16,7 @@ import matplotlib.pyplot as plt
 class bert_process:
 
     def __init__(self, aclarc_data:list, scicite_data=None, confidence_level:float=1, cite2sentence_percent:float=0.15, max_len:int=300, 
-    batch_size:int=1, shuffle:bool=True, pretrained_model_name:str='bert-base-uncased', padding:str='max_length'):
+    batch_size:int=1, shuffle:bool=True, pretrained_model_name:str='bert-base-uncased', padding:str='max_length', repeat:list=[1]*6):
         
         self.data = aclarc_data
         self.scicite_data = scicite_data
@@ -28,7 +29,11 @@ class bert_process:
 
         self.max_len = max_len
         self.padding = padding
+        # tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_uncased')
+        # model = AutoModel.from_pretrained('allenai/scibert_scivocab_uncased')
         self.tokenizer = BertTokenizer.from_pretrained(pretrained_model_name)
+        # print(self.tokenizer(['[SEP]']))
+        # self.tokenizer = BertTokenizer.from_pretrained(pretrained_model_name)
         self.citation_id = torch.tensor(11091) # id for citation
         self.sep_id = torch.tensor(102) # id for [SEP]
         self.cite_pos = [] #citation pisition
@@ -41,12 +46,18 @@ class bert_process:
         #  {'0': 3328, '1': 317, '2': 460, '3': 63, '4': 76, '5': 60}
         # {'Background':3, 'Uses':1, 'CompareOrContrast':2, 'Extends':4, 'Motivation':0, 'Future':5}
         # self.output_types2idx = {'Background':0, 'Uses':1, 'CompareOrContrast':2, 'Extends':3, 'Motivation':4, 'Future':5}
-        self.output_types2idx = {'Background':2, 'Uses':5, 'CompareOrContrast':1, 'Extends':4, 'Motivation':3, 'Future':0}
+        self.output_types2idx = {'Background':0, 'Uses':1, 'CompareOrContrast':2, 'Extends':3, 'Motivation':4, 'Future':5}
+
+        # self.output_types2idx = {'Background':2, 'Uses':4, 'CompareOrContrast':1, 'Extends':5, 'Motivation':3, 'Future':0}
         self.mask = None
         self.token_type_ids = None
 
+        self.repeat = repeat
+
+
         if self.scicite_data:
             self.clean_add_scicite_data()
+        self.repeat_minority()
         self.index_input()
         self.index_output()
         self.make_data_loader()
@@ -54,26 +65,15 @@ class bert_process:
 
     """
         # acl-arc data:
-
         # keys:
         # ['text', 'citing_paper_id', 'cited_paper_id', 'citing_paper_year', 'cited_paper_year', 'citing_paper_title', 'cited_paper_title', 'cited_author_ids', 'citing_author_ids', 'extended_context', 
         # 'section_number', 'section_title', 'intent', 'cite_marker_offset', 'sents_before', 'sents_after', 'cleaned_cite_text', 'citation_id', 'citation_excerpt_index', 'section_name']
-
         # relevant keys: ['text','extended_context','intent','cleaned_cite_text','section_name']
-
         # 'section_name': {'introduction' , 'experiments', None , 'conclusion', 'related work', 'method'}
-
         # 'intent': {'Background', 'Uses', 'CompareOrContrast', 'Extends', 'Motivation', 'Future'}
-
-
-
-
         # scicite data:
-
         # keys: ['source', 'citeEnd', 'sectionName', 'citeStart', 'string', 'label', 'label_confidence', 'citingPaperId', 'citedPaperId', 'isKeyCitation', 'id', 'unique_id', 'excerpt_index']
-
         # relevant keys: ['citeEnd', 'sectionName', 'citeStart', 'string', 'label', 'label_confidence']
-
         # 'sectionName': 
         # Counter({'Discussion': 1240, 'Introduction': 834, 'Methods': 800, '': 587, 'DISCUSSION': 483, 'Results': 359, '1. Introduction': 349, 'METHODS': 296, 'INTRODUCTION': 263, '4. Discussion': 172, 'RESULTS': 162, 
         # '1 Introduction': 160, 'Background': 101, 'Method': 87, 'Results and discussion': 74, '2. Methods': 60, '1. INTRODUCTION': 54, 'Results and Discussion': 41, 'Methodology': 37, 'Materials and methods': 35, 
@@ -87,10 +87,7 @@ class bert_process:
         # '3. EXPERIMENTS': 5, 'MBD': 5, '3.1. Depression': 5, 'Implications for Explaining Cooperation: Towards a Classification in Terms of Psychological Mechanisms': 4, 'Interpretation Bias in Social Anxiety': 4, '7 Experiments': 4,
         # '4 EXPERIMENTS': 4, '5.2. Implications for Formation of Crustal Plateau Structures': 4, '7 Numerical experiments': 4, 'Changes in CO2 and O3 Effects Through Time': 4, '3. A REAL OPTION TO INVEST UNDER CHOQUET-BROWNIAN AMBIGUITY': 4,
         # .........
-
-
         # 'label': Counter({'background': 4840, 'method': 2294, 'result': 1109})
-
     """
     
 
@@ -148,6 +145,13 @@ class bert_process:
                 self.data.append(exa)
 
 
+    def repeat_minority(self):
+        repeated = []
+        for exa in self.data:
+            for _ in range(self.repeat[self.output_types2idx[exa['intent']]]-1):
+                repeated.append(exa)
+        self.data += repeated
+
 
     def index_input(self):
         
@@ -172,15 +176,6 @@ class bert_process:
     def index_output(self):
         self.indexed_output = np.array([self.output_types2idx[exa['intent']] for exa in self.data], dtype=np.int32)
 
-        # c = 0
-        # self.indexed_output = np.array([], dtype=np.int32)
-
-        # for exa in self.data:
-        #     w = exa['intent']
-        #     if self.output_types2idx.get(w, -1) == -1:
-        #         self.output_types2idx[w] = c
-        #         c += 1
-        #     self.indexed_output = np.append(self.indexed_output, self.output_types2idx[w])
 
 
     def make_data_loader(self):
@@ -196,11 +191,13 @@ def load_data(path):
 
 
 
-# ACL_TRAIN_PATH = './ACL-ARC/train.jsonl'
-# ACL_TEST_PATH = './ACL-ARC/test.jsonl'
-# ACL_DEV_PATH = './ACL-ARC/dev.jsonl'
+# ACL_TRAIN_PATH = './acl-arc/train.jsonl'
+# ACL_TEST_PATH = './acl-arc/test.jsonl'
+# ACL_DEV_PATH = './acl-arc/dev.jsonl'
 
 # train_data, test_data, dev_data = load_data(ACL_TRAIN_PATH), load_data(ACL_TEST_PATH), load_data(ACL_DEV_PATH)
+
+# print(len(train_data))
 
 
 
@@ -214,29 +211,24 @@ def load_data(path):
 # 6k has label confidence
 # 4.3k label confidence >= 0.75
 # 3.4k label confidence = 1
-# whole='dasd dsa ds ly (Dymkowska et al. sfef fe '
-# place='ly (Dymkowska et al.'
-# a = whole[:12] + "@@CITATION" + whole[32:]
-# print(whole[12:32])
-# print(a)
+
 # tokenizer = BertTokenizer.from_pretrained('bert-large-uncased')
 # model = BertModel.from_pretrained('bert-large-uncased')
+
 # # text = train_data[0]['string']
 # # # text='CITATION'
 # # text='[SEP]'
 # text = "Typical examples are Bulgarian ( @Citation@ ; Simov and Osenova , 2003 ) , [SEP] Chinese ( Chen et al. , 2003 ) , Danish ( Kromann , 2003 ) , and Swedish ( Nilsson et al. , 2005 ) . Second Sentence is here as well ."
-# # # text = ["Typical examples are Bulgarian ( @Citation@ ; Simov and Osenova , 2003 ) , Chinese ( Chen et al. , 2003 ) , Danish ( Kromann , 2003 ) , and Swedish ( Nilsson et al. , 2005 ) . Second Sentence is here as well .",
-# # # "Typical examples are Bulgarian ( @Citation@ ; Simov and Osenova , 2003 ) , Chinese ( Chen et al. , 2003 ) , Danish ( Kromann , 2003 ) , and Swedish ( Nilsson et al. , 2005 ) . Second Sentence is here as well ."]
+
 # # print(text)
+
 # encoded_input = tokenizer(text, padding='max_length', max_length=100)
+
 # for key,val in encoded_input.items():
 #     print(key,val)
 # # print(tokenizer.decode(encoded_input['input_ids']))
 
 
-# # Convert token to vocabulary indices
-# # indexed_tokens = tokenizer.convert_tokens_to_ids(encoded_input)
-# # print(indexed_tokens)
 
 
 # aa = bert_process(None,train_data)
@@ -272,20 +264,13 @@ class Dataset:
 
 
 """
-
 class glove_process:
-
-
     def __init__(self, data:list[dict], input_key:list[str]=['section_name', 'cleaned_cite_text'], min_freq:int=1, max_len:int=300, 
                 batch_size:int=1, shuffle:bool=True, glove_name:str="6B", glove_dim:int=100):
-
-
         self.data = data # a list of dictionaries, one for one example/data point
         self.num_example = len(data) # number of data points in train/test/dev set
-
         self.max_len = max_len # for batching, the max length of input sequence of one sample
         self.min_freq = min_freq # min frequency for a word, set to unk if < min frequency
-
         self.input_key = input_key # which dict keys of raw data to use to construct input sequence
         self.input_word_counter = Counter()
         self.input_types = np.array(["<UNK>"]) # vocab, initialize with UNK
@@ -294,29 +279,21 @@ class glove_process:
         self.padded_input = torch.zeros(self.num_example, self.max_len) # (number of samples, max_len)
         self.mask = torch.ones_like(self.padded_input) # (number of samples, max_len)
         self.padding_idx = []  # (number of samples, various length)
-
         self.output_key = 'intent'
         self.output_word_counter = Counter()
         self.output_types = np.array([])
         self.output_types2idx = {}
         self.indexed_output = []
-
         self.batch_size = batch_size # for data loader
         self.shuffle = shuffle
         self.dataset = None
         self.data_loader = None
-
         self.glove_vectors = GloVe(name=glove_name, dim=glove_dim)
         self.pretrained_embedding = self.glove_vectors.get_vecs_by_tokens("<UNK>").view(1,-1) # initialize with UNK
         self.V = None
-
         # self.run()
-
-
-
     # make word counters for input sequences and output words in data
     def make_counters(self):
-
         for example in self.data:
             # for input
             for key in self.input_key: 
@@ -324,27 +301,14 @@ class glove_process:
                 self.input_word_counter.update(self.split(string.lower())) 
             # for output
             self.output_word_counter.update([example[self.output_key]])
-
-
-
     def split(self, x:str):
-
         splitted = re.split(' |-', x) # split by white space or dash; '|' can hold multiple delimiters
         out = [w[:4] if (len(w)==5 and w[:4].isnumeric()) else w for w in splitted] # modify some year-related strings: 1997a -> 1997
         return out
-
-
-
     def build_output_vocab(self): 
-
         self.output_types = [word for (word, count) in self.output_word_counter.most_common()]
         self.output_types2idx = {word: i for i, word in enumerate(self.output_types)}
-
-
-
-
     def index_data(self):
-
         for example in self.data:
             
             # input
@@ -355,38 +319,24 @@ class glove_process:
             x = x[1:] # remove first space
             indexed_x = [self.input_types2idx.get(w, self.input_types2idx["<UNK>"]) for w in self.split(x.lower())] # set to UNK if not in vocab that was built
             self.indexed_input.append(torch.tensor(indexed_x))
-
             # output
             y = example[self.output_key]
             self.indexed_output.append(self.output_types2idx[y])
-
-
-
-
     def input_padding(self):
-
         for i, example in enumerate(self.indexed_input):
             example = example[:self.max_len] # truncate if excced length; does nothing otherwise
             l = len(example)
             self.padded_input[i,:l] = example
             self.mask[i,l:] = 0     # broadcast to all 0
             self.padding_idx.append(torch.tensor([k for k in range(l, self.max_len)]))
-
-
     def prepare_dataset(self):
-
         self.dataset = Dataset(self.padded_input, self.indexed_output, self.mask)
         self.data_loader = torch.utils.data.DataLoader(self.dataset, batch_size=self.batch_size, shuffle=self.shuffle)
-
-
     def run(self):
-
         self.make_counters()
         self.build_input_vocab()
         self.build_output_vocab()
         self.index_data()
         self.input_padding()
         self.prepare_dataset()
-
-
 """
