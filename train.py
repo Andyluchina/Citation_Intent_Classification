7eae3ce97c858168888b8d24a5486d889111a732
Andy Lu <qingjielu990916@gmail.com>
2022-12-24 00:28:59 +0800

    cosine difference

--- train.py
+++ train.py
@@ -140,15 +140,15 @@ network = CustomBertClassifier(hidden_dim= 100, bert_dim_size=bert_dim_size, num
 
 loss_fn = nn.NLLLoss()
 
-optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-5, lr=0.001)
+optimizer = torch.optim.Adam(network.parameters(), weight_decay = 1e-6, lr=0.001)
 # optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
 
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience = 2, factor = 0.5, verbose = True)
 n_epochs = 80
-class_factor = 1.6
+class_factor = 1.7
 sum_factor = 0.8
 normalizing_factor = 0.5
-accuracy_factor = 1.2
+accuracy_factor = 1.1
 
 
 pytorch_total_params = sum(p.numel() for p in network.parameters())
