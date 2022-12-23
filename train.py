c7547ec6b837c46e925a39df324118650668e13e
Andy Lu <qingjielu990916@gmail.com>
2022-12-23 23:50:39 +0800

    cosine difference

--- train.py
+++ train.py
@@ -129,7 +129,7 @@ output_matrix = bert_output[torch.arange(bert_output.shape[0]), 1]
 
 print("generating output_matrix of shape")
 output_matrix.require_grad = False
-output_matrix = output_matrix.to(device)
+output_matrix = output_matrix.to('cpu')
 print(output_matrix.shape)
 
 del scibert
